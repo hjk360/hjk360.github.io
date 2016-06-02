8e0cc1f45410b5e3a395edf85421620ec6000c38
TaiKongcang <2140108747@qq.com>
2016-06-02 13:37:44 +0800

    Delete baidutongji.js

--- js/baidutongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-	(function() {
-	  var hm = document.createElement("script");
-	  hm.src = "//hm.baidu.com/hm.js?721eed6abff09b0a538eb2c9c7b60b86";
-	  var s = document.getElementsByTagName("script")[0]; 
-	  s.parentNode.insertBefore(hm, s);
-	})();
